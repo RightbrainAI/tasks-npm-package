5f1d916f761acef2638973acb893c5d7c9ceae22
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-10-22 12:25:20 +0500

    fix: rollup config to support commonJs modules

--- rollup.dist.config.js
+++ rollup.dist.config.js
@@ -1,40 +1,41 @@
 import { babel, getBabelOutputPlugin } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 
-export default [{
-  input: 'lib/index.js',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'es',
-      sourcemap: true
-    },
-    {
-      file: 'dist/index.es5.js',
-      format: 'es',
-      plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] })],
-      sourcemap: true
-    },
-    {
-      file: 'dist/index.cjs.js',
-      format: 'cjs',
-      plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] })],
-      sourcemap: true
-    }
-  ]
-}, {
-  input: 'lib/index.js',
-  output: {
-    file: 'dist/index.iife.js',
-    format: 'iife',
-    name: 'index',
-    sourcemap: true
+/**
+ * @type {import("rollup").RollupOptions}
+ */
+export default [
+  {
+    input: 'lib/index.js',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'es',
+        plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] })],
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] })],
+        sourcemap: true,
+      },
+    ],
   },
-  plugins: [
-    commonjs(),
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'bundled'
-    })
-  ]
-}]
+  {
+    input: 'lib/index.js',
+    output: {
+      file: 'dist/index.iife.js',
+      format: 'iife',
+      name: 'index',
+      sourcemap: true,
+    },
+    plugins: [
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+      }),
+    ],
+  },
+]
